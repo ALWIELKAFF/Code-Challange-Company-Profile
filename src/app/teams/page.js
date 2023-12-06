d7efc32048f0616700a62645422c1e7e1d7bb5e6
ALWIELKAFF <mynamealwikaff@gmail.com>
2023-12-06 13:34:48 +0700

    ganti

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -8,7 +8,7 @@ import { NavBar } from "@/components/NavBar";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://randomuser.me/api/?results=5");
+  const res = await fetch("https://randomuser.me/api/?results=4");
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
